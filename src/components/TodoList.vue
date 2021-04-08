f205c3c30f82b98500cb2b2fc67a24e7e10a6502
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-04-08 21:53:08 +0900

    methods added&modifed

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,8 @@
   <div>
      <ul>
        <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-         {{todoItem}}
+         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted : todoItem.completed}" v-on:click="toggleComplete(todoItem,index)" ></i>
+         <span v-bind:class="{textCompleted : todoItem.completed}">{{todoItem.item}}</span>
          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
             <i class="fas fa-trash-alt"></i>
          </span>
@@ -18,29 +19,35 @@ export default {
       todoItems : [],
     }
   },
-  //vue lifecycle : vue instance-> mounted-> updated-> destroyed (총 10개 정도)
+ 
   created : function(){
     if(localStorage.length > 0 ){
       for(var i=0; i < localStorage.length; i++){
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          console.log(localStorage.key(i))
-            this.todoItems.push(localStorage.key(i));
+            //console.log(localStorage.key(i))
+            //this.todoItems.push(localStorage.key(i));
+            //localStorage.getItem(localStorage(i));
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
-
       }
     }
   },
   methods : {
     removeTodo : function(todoItem, index){
-      console.log(todoItem, index);
+      //console.log(todoItem, index);
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index, 1);
-    }
+    },
+    toggleComplete : function(todoItem, ){
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
   }
 }
 </script>
 
-<style>
+<style scoped>
 ul {
   list-style-type : none;
   padding-left : 0px;
